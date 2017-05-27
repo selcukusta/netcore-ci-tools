2078e6f40353dbee9141a2b1f2aec88eabfb3c70
Selçuk Usta <susta@hurriyet.com.tr>
2017-05-27 21:16:13 +0300

    Bug fixing

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,5 @@ dotnet restore
 dotnet build --configuration Release
 dotnet xunit -configuration Release -xml ../../shippable/xunit-result/result.xml
 cd ../..
-xsltproc -o shippable/testresults/result-junit.xml shippable/transform-files/xunit-to-junit.xslt shippable/xunit-results/result.xml
+xsltproc -o shippable/testresults/result-junit.xml shippable/transform-file/xunit-to-junit.xslt shippable/xunit-result/result.xml
 #dotnet test NetCoreTestCI.Test.csproj --configuration Release --logger:trx;LogFileName=../../../shippable/testresults/TestResult.xml
