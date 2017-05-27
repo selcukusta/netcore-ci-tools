de9b0cfba4ec2b20a6326c7baad198006a9b6d49
Selçuk Usta <susta@hurriyet.com.tr>
2017-05-27 20:42:42 +0300

    Change test framework

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+apt-get --assume-yes install xsltproc
 cd ./src/NetCoreTestCI.Test
 dotnet restore
 dotnet build --configuration Release
-dotnet test NetCoreTestCI.Test.csproj --configuration Release --logger:trx;LogFileName=../../../shippable/testresults/TestResult.xml
+dotnet xunit -configuration Release -xml ../../shippable/xunit-result/result.xml
+cd ../..
+xsltproc -o shippable/xunit-results/result.xml shippable/transform-files/xunit-to-junit.xslt shippable/testresults/result-junit.xml
+#dotnet test NetCoreTestCI.Test.csproj --configuration Release --logger:trx;LogFileName=../../../shippable/testresults/TestResult.xml
--- src/NetCoreTestCI.Test/TextTest.cs
+++ src/NetCoreTestCI.Test/TextTest.cs
@@ -1,28 +1,17 @@
-using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetCoreTestCI.Framework;
+using Xunit;
 
 namespace NetCoreTestCI.Test
 {
-    [TestClass]
     public class TextTest
     {
-        [TestMethod]
-        public void TestSlugify()
+        [Theory]
+        [InlineData("Hello .Net Core!", "hello-net-core")]
+        [InlineData("Türkçe karakterler çok şanssız!", "turkce-karakterler-cok-sanssiz")]
+        public void TestSlugify(string actual, string expected)
         {
-            var input = "Hello .Net Core!";
-            var expected = "hello-net-core";
-            var result = Text.Slugify(input);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TestSlugifyWithTurkishChars()
-        {
-            var input = "Türkçe karakterler çok şanslı!";
-            var expected = "turkce-karakterler-cok-sansli";
-            var result = Text.Slugify(input);
-            Assert.AreEqual(expected, result);
+            var result = Text.Slugify(actual);
+            Assert.Equal<string>(expected, result);
         }
     }
 }
